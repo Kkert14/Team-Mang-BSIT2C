cd25debfe4a5fa04892457e1033834b88c0af2c4
nicoleelera-droidw <ashleynicoleelera@gmail.com>
2026-02-18 10:15:29 +0800

    added Profiling

--- app/Views/theme/sidebar.php
+++ app/Views/theme/sidebar.php
@@ -94,11 +94,17 @@ body.dark-mode .main-sidebar .nav-link:hover {
       </a>
     </li>
      <li class="nav-item">
+      <a href="<?= base_url('profiling') ?>" class="nav-link <?= is_active(1, 'profiling') ?>">
+        <i class="nav-icon fas fa-address-book"></i>
+        <p>Profiling</p>
+      </a>
+    </li>
+    </li>
+     <li class="nav-item">
       <a href="<?= base_url('users') ?>" class="nav-link <?= is_active(1, 'users') ?>">
         <i class="nav-icon fas fa-user-lock"></i>
         <p>User Accounts</p>
       </a>
-    </li>
   </ul>
 </nav>
 </div>
