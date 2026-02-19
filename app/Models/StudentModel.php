e287e98274b2c99c70404a8a461f34735327c26e
Bonnakid <rafbonmig@gmail.com>
2026-02-19 20:27:57 +0800

    Added StudentModel in Models

---
+++ app/Models/StudentModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class StudentModel extends Model
+{
+    protected $table = 'students';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = ['name', 'year_level', 'course', 'section'];
+
+    public function getRecords($start, $length, $searchValue = '')
+    {
+        $builder = $this->builder();
+        $builder->select('*');
+
+        if (!empty($searchValue)) {
+            $builder->groupStart()
+                ->orLike('name', $searchValue)
+                ->groupEnd();
+        }
+
+        // Clone builder for filtered count before applying limit
+        $filteredBuilder = clone $builder;
+        $filteredRecords = $filteredBuilder->countAllResults();
+
+        $builder->limit($length, $start);
+        $data = $builder->get()->getResultArray();
+
+        return ['data' => $data, 'filtered' => $filteredRecords];
+    }
+}
